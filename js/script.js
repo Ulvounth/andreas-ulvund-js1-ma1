43d0a9c48b7c16e4837620eac6da6affb147e5d0
Andreas Ulvund <ulvund_a@hotmail.com>
2022-11-01 14:21:26 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,3 +11,36 @@ const cats = [
     age: 21,
   },
 ];
+
+// Question 1
+
+let cat = {
+  complain: function () {
+    console.log("Meow!");
+  },
+};
+cat.complain();
+
+// Question 2
+
+let heading = document.querySelector("h3");
+
+heading.innerHTML = "Updated heading";
+
+// Question 3
+
+heading.style.fontSize = "2em";
+
+// Question 4
+
+heading.className = "Subheading";
+
+// Question 5
+
+let paragraphs = document.querySelectorAll("p");
+
+for (let i = 0; i < paragraphs.length; i++) {
+  paragraphs[i].style.color = "red";
+}
+
+// Question 6
